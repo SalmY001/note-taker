bff8579fc53686ba3a1fda589b12f75416c2b657
SalmY001 <80605132+SalmY001@users.noreply.github.com>
2023-04-25 23:18:53 +0100

    update to get save/delete/id working

--- apiroutes.js
+++ apiroutes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
-const data = require('./db.json')
 const fs = require('fs')
 const util = require('util')
 const readFileAsync = util.promisify(fs.readFile)
 const writeFileAsync = util.promisify(fs.writeFile)
+
+const uuid = require('uuid');
+//const {uuidv4} = require('uuid');
 
 // Get route for api notes
 router.get('/notes', async function(req, res){
@@ -15,13 +17,31 @@ router.get('/notes', async function(req, res){
 
 // Post route for api notes
 router.post('/notes', async function(req, res){
-    const notes = await readFileAsync('db.json', 'UTF-8')
-    notes.push(req.body)
-    /*notes.push(notes)*/
+    /*const notes = await readFileAsync('db.json', 'UTF-8')*/
+
+    /*notes.push(req.body)
+    notes.push(notes)
     writeFileAsync('db.json', notes, 'UTF-8')
     res.json(JSON.parse(notes))
-    console.log('New note added: '+notes.title);
+    console.log('New note added: '+notes.title);*/
+    console.log('Checking router');
+
+    const {title, text} = (req.body);
+
+    if (req.body) {
+        const newNotes = {
+            title, 
+            text,
+            id: uuid.v4(),
+        };
+
+        readAndAppend(newNotes, 'db.json');
+        res.json(`Note added successfully`)
+    } else {
+    res.error('Error in adding note.');
+  }
 });
+
 
 // Get note including an ID
 router.get('/notes/:id', async function(req, res){
@@ -31,19 +51,44 @@ router.get('/notes/:id', async function(req, res){
 
 // Delete note including an ID
 router.delete('/notes/:id', async function(req, res){
-    notes.splice(req.params.id, 1);
-    refreshDb;
+    fs.readFile('db.json', 'UTF-8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          let parsedData = JSON.parse(data);
+          parsedData = parsedData.filter(note => note.id != req.params.id) 
+    refreshDb(parsedData);
     console.log("Deleted note: "+req.params.id);
+    res.status(200).json("Deleted note: "+ req.params.id)
+    }})
+
 });
 
 
 // Updates the json file whenever a note is added or deleted
-function refreshDb() {
-    writeFileAsync('/db.json',JSON.stringify(notes, '\t'), err => {
+function refreshDb(notes) {
+    writeFileAsync('db.json',JSON.stringify(notes, '\t'), err => {
         if (err) throw err;
         return true;
     });
-
 }
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'UTF-8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        writeToFile(file, parsedData);
+      }
+    });
+};
+
+const writeToFile = (destination, content) =>
+fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+err ? console.error(err) : console.info(`\nData written to ${destination}`)
+);
+
 
 module.exports = router;
