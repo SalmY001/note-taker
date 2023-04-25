4847b37be0ad728304bc5b31f25e5a2b723db40c
SalmY001 <80605132+SalmY001@users.noreply.github.com>
2023-04-25 15:34:57 +0100

    updated code include delete note

--- apiroutes.js
+++ apiroutes.js
@@ -5,13 +5,36 @@ const util = require('util')
 const readFileAsync = util.promisify(fs.readFile)
 const writeFileAsync = util.promisify(fs.writeFile)
 
+// Get route for api notes
 router.get('/notes', async function(req, res){
-    const notes = await readFileAsync('db.json', 'utf-8')
+    const notes = await readFileAsync('db.json', 'UTF-8')
     console.log("testing")
     res.json(JSON.parse(notes))
 
-})
+});
 
+// Post route for api notes
+router.post('/notes', async function(req, res){
+    const notes = await readFileAsync('db.json', 'UTF-8')
+    notes.push(req.body)
+    /*notes.push(notes)*/
+    writeFileAsync('db.json', notes, 'UTF-8')
+    res.json(JSON.parse(notes))
+    console.log('New note added: '+notes.title);
+});
+
+// Get note including an ID
+router.get('/notes/:id', async function(req, res){
+    console.log("testing1")
+    res.json(JSON.parse(notes[req.params.id]));
+});
+
+// Delete note including an ID
+router.delete('/notes/:id', async function(req, res){
+    notes.splice(req.params.id, 1);
+    res.json(JSON.parse(notes[req.params.id]));
+    console.log("Deleted note: "+req.params.id);
+});
 
 
 
